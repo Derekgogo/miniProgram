cf3e0ef3399f6b0a4da2f75f7f313a145144ea6e
derek <2514838942@qq.com>
2020-02-07 17:08:26 +0800

    2020-2-7小程序的初体验

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,66 +1,27 @@
-// miniprogram/pages/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    name:'1111',
+    age:18,
+    student:[
+      { id: 1, name: '111', age: 30 },
+      { id: 2, name: '222', age: 30 },
+      { id: 3, name: '333', age: 30 },
+      { id: 4, name: '444', age: 30 }
+    ],
+    counter:0
   },
+  handleBtnClick(){
+    //1错误的做法:界面是不会刷新的
+    //  this.data.counter += 1;
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    // 2.this.setData()
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction(){
+    this.setData({
+      counter:this.data.counter - 1
+    }) 
   }
 })
