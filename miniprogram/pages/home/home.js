1c12d5a4d04aa8fc1cfc54e4a34e3c3b37712707
derek <2514838942@qq.com>
2020-02-11 14:39:21 +0800

    小程序展示弹窗

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,66 +1,49 @@
 // miniprogram/pages/home.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+  handleShowToast() {
+    wx.showToast({
+      title: '你好你好',
+    })
+  },
+  handleShowModal() {
+    wx.showModal({
+      title: '哈哈好',
+      content: 'hehhe',
+      showCancel: false, //隐藏取消按钮
+      success: res => {
+        if (res.cancel) {
+          console.log(res);
+        }
+        if (res.confirm) {
+          console.log(res);
+        }
 
+      }
+    })
+  },
+  //发送网络请求成功进行返回数据或跳转页面
+  handleShowLoading() {
+    wx.showLoading({
+      title: '加载中',
+      mask: true,
+      success: res => {
+        console.log(res);
+      }
+    })
+    setTimeout(() => {
+      wx.hideLoading();
+    }, 2000);
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleShowActionSheet(){
+    wx.showActionSheet({
+      itemList: ['A','B','C'],
+      itemColor:'red',
+      success:res=>{
+        console.log(res);
+      }
+    })
   }
+
 })
