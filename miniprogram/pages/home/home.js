f45b399b609dd7d20cdb035c19e1e4b18afc2a82
derek <2514838942@qq.com>
2020-02-11 13:51:14 +0800

    网络请求函数封装

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,4 +1,4 @@
-// miniprogram/pages/home.js
+import request from '../../service/network.js';
 Page({
 
   /**
@@ -11,56 +11,25 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+    //1.源生的方式发送网络请求
+    // this.get_data_origin();
 
+    // 2.使用封装的request发送网络请求
+    //Promise最大的好处就是防止出现回调地狱
+    request({
+      url: 'http://123.207.32.32:8000/recommend',
+    })
+      // .then(res => {
+
+      // }).catch(err => {
+
+      // })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  get_data_origin() {
+    // wx.request({
+    //   url: '',
+    // })
   }
+
 })
