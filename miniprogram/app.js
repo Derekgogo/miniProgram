95f03d45ac5fdb09f0e3ed8feb9aacbe16c5f1c9
derek <2514838942@qq.com>
2020-02-11 18:04:57 +0800

    小程序登录演练

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,20 +1,68 @@
+const TOKEN = 'token';
 //app.js
 App({
-  onLaunch: function () {
-    
-    if (!wx.cloud) {
-      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
-    } else {
-      wx.cloud.init({
-        // env 参数说明：
-        //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
-        //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
-        //   如不填则使用默认环境（第一个创建的环境）
-        // env: 'my-env-id',
-        traceUser: true,
-      })
+  //对象: 小程序关闭掉内存就会被回收   对象放在内存中
+  globalData: {
+    token: ''
+  },
+  onLaunch: function() {
+    //1.判断缓存中取出token是否登录
+    const token = wx.getStorageSync(TOKEN);
+    //2.判断token是否有值
+    if (token && token.length !== 0) {
+      this.checkToken(token);
+    } else { //没有token 进行登录操作
+      this.login();
     }
 
-    this.globalData = {}
+  },
+  //检查token是否过期
+  checkToken(token) {
+    wx.request({
+      url: 'http://123.207.32.32:3000/auth',
+      method: 'post',
+      header: {
+        token
+      },
+      success: res => {
+        if (!res.data.errCode) {
+          console.log('token有效');
+          this.globalData.token = token;
+        }
+      },
+      fail: res => {
+        console.log(res);
+      }
+    })
+  },
+  login() {
+    console.log('登录');
+    //登录操作
+    wx.login({
+      //code只有5分钟的有效期
+      success: res => {
+        //1.获取code
+        const code = res.code;
+
+        //  2.将code发送给我们的服务器
+        wx.request({
+          url: 'http://123.207.32.32:3000/login',
+          method: 'post',
+          data: {
+            code
+          },
+          success: res => {
+            //1.取出token
+            const token = res.data.token;
+
+            //2.将token保存在globalData中
+            this.globalData.token = token;
+
+            //3.进行本地存储
+            wx.setStorageSync(TOKEN, token);
+          }
+        })
+      }
+    })
   }
 })
--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,3 +1,4 @@
+const app = getApp();
 // miniprogram/pages/home.js
 Page({
 
